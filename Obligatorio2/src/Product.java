35663fdbdb3bb4ac4b85526058ca4b5d59fe8020
Snyczka <nyczkasanti@gmail.com>
2024-09-12 14:38:34 -0300

    added product class, removed unnecessary ExecutorService interface

---
+++ Obligatorio2/src/Product.java
@@ -0,0 +1,44 @@
+
+
+public class Product {
+
+    private int id;
+    private String name;
+    private float price;
+    private int stock;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    
+}
